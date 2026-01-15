a4069f03b47f8e14d90904fdfbf1d6c1fdd4411c
ItsPouria <pouriamemarvar@gmail.com>
2026-01-15 18:20:48 +0100

    add structs for Player and CameraSensitivity components

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,20 @@ fn main() {
     App::new().add_plugins(DefaultPlugins).run();
 }
 
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct CameraSensitivity(Vec2);
+
+impl Default for CameraSensitivity {
+    fn default() -> Self {
+        Self(Vec2::new(0.003, 0.002))
+    }
+}
+#[derive(Component)]
+struct WorldModelCamera;
+
+const DEFAULT_RENDER_LAYER: usize = 0;
+const VIEW_MODEL_RENDER_LAYER: usize = 1;
+
