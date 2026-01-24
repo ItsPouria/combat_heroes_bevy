d7c0bf23c572fcec35d2fe4ddc78746b1e76072b
ItsPouria <pouriamemarvar@gmail.com>
2026-01-24 21:10:52 +0100

    add structure to separate different systems into plugins

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ use bevy::{
     camera::visibility::RenderLayers, color::palettes::tailwind,
     input::mouse::AccumulatedMouseMotion, light::NotShadowCaster,
 };
+use bevy_inspector_egui::bevy_egui::EguiPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+mod plugins;
+use plugins::player::PlayerPlugin;
+
+const DEFAULT_RENDER_LAYER: usize = 0;
+const VIEW_MODEL_RENDER_LAYER: usize = 1;
 
 fn main() {
     App::new()
@@ -17,86 +25,13 @@ fn main() {
             }),
             ..default()
         }))
-        .add_systems(Startup, (spawn_view_model, spawn_lights, generate_dungeon))
-        .add_systems(Update, (mouse_movement))
+        .add_plugins((
+            EguiPlugin::default(),
+            WorldInspectorPlugin::new(),
+            PlayerPlugin,
+        ))
+        .add_systems(Startup, (spawn_lights, generate_dungeon))
         .run();
-}
-
-#[derive(Component)]
-struct Player;
-
-#[derive(Debug, Component, Deref, DerefMut)]
-struct CameraSensitivity(Vec2);
-
-impl Default for CameraSensitivity {
-    fn default() -> Self {
-        Self(Vec2::new(0.003, 0.002))
-    }
-}
-#[derive(Component)]
-struct WorldModelCamera;
-
-#[derive(Component, Deref, DerefMut)]
-struct PlayerMovementSpeed(f32);
-
-impl Default for PlayerMovementSpeed {
-    fn default() -> Self {
-        Self(5.0)
-    }
-}
-
-#[derive(Debug, Component)]
-struct RoomModule {
-    sockets: [i8; 4],
-    allowed_neighbours: [i8; 4],
-}
-
-const DEFAULT_RENDER_LAYER: usize = 0;
-const VIEW_MODEL_RENDER_LAYER: usize = 1;
-
-fn spawn_view_model(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let arm = meshes.add(Cuboid::new(0.05, 0.05, 0.5));
-    let arm_material = materials.add(Color::from(tailwind::TEAL_200));
-
-    commands.spawn((
-        Player,
-        CameraSensitivity::default(),
-        Transform::from_xyz(0.0, 1.0, 0.0),
-        Visibility::default(),
-        children![
-            (
-                WorldModelCamera,
-                Camera3d::default(),
-                Projection::from(PerspectiveProjection {
-                    fov: 90.0_f32.to_radians(),
-                    ..default()
-                }),
-            ),
-            (
-                Camera3d::default(),
-                Camera {
-                    order: 1,
-                    ..default()
-                },
-                Projection::from(PerspectiveProjection {
-                    fov: 70.0_f32.to_radians(),
-                    ..default()
-                }),
-                RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
-            ),
-            (
-                Mesh3d(arm),
-                MeshMaterial3d(arm_material),
-                Transform::from_xyz(0.2, -0.1, -0.5).with_rotation(Quat::from_rotation_x(PI / 3.0)),
-                RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
-                NotShadowCaster,
-            ),
-        ],
-    ));
 }
 
 fn spawn_lights(mut commands: Commands) {
@@ -111,36 +46,30 @@ fn spawn_lights(mut commands: Commands) {
     ));
 }
 
-fn mouse_movement(
-    accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
-    player: Single<(&mut Transform, &CameraSensitivity), With<Player>>,
-) {
-    let (mut transform, camera_sensitivity) = player.into_inner();
-    let delta = accumulated_mouse_motion.delta;
-
-    if delta != Vec2::ZERO {
-        let delta_yaw = -delta.x * camera_sensitivity.x;
-        let delta_pitch = -delta.y * camera_sensitivity.y;
-
-        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
-        let yaw = yaw + delta_yaw;
-        const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
-        let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
-        transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
-    }
-}
-
 fn generate_dungeon(
     mut meshes: ResMut<Assets<Mesh>>,
     mut commands: Commands,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let floor = meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(1.0)));
+    let wall_left = meshes.add(Plane3d::new(Vec3::X, Vec2::splat(10.0)));
+    let wall_right = meshes.add(Plane3d::new(-Vec3::X, Vec2::splat(10.0)));
     let materials = materials.add(Color::WHITE);
 
     commands.spawn((
         Mesh3d(floor),
         MeshMaterial3d(materials.clone()),
-        Transform::from_xyz(0.0, 0.0, 3.0),
+        Transform::from_xyz(0.0, 0.0, 0.0),
+    ));
+
+    commands.spawn((
+        Mesh3d(wall_left),
+        MeshMaterial3d(materials.clone()),
+        Transform::from_xyz(-5.0, 5.0, 5.0),
+    ));
+    commands.spawn((
+        Mesh3d(wall_right),
+        MeshMaterial3d(materials.clone()),
+        Transform::from_xyz(5.0, 5.0, 5.0),
     ));
 }
---
+++ src/plugins/player.rs
@@ -0,0 +1,98 @@
+use bevy::camera::visibility::RenderLayers;
+use bevy::color::palettes::tailwind;
+use bevy::input::mouse::AccumulatedMouseMotion;
+use bevy::light::NotShadowCaster;
+use bevy::prelude::*;
+use std::f32::consts::{FRAC_PI_2, PI};
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_view_model)
+            .add_systems(Update, mouse_movement);
+    }
+}
+
+// --- Components ---
+#[derive(Component)]
+pub struct Player;
+
+#[derive(Debug, Component, Deref, DerefMut)]
+struct CameraSensitivity(Vec2);
+
+impl Default for CameraSensitivity {
+    fn default() -> Self {
+        Self(Vec2::new(0.003, 0.002))
+    }
+}
+
+#[derive(Component)]
+struct WorldModelCamera;
+
+const VIEW_MODEL_RENDER_LAYER: usize = 1;
+
+// --- Systems ---
+fn spawn_view_model(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let arm = meshes.add(Cuboid::new(0.05, 0.05, 0.5));
+    let arm_material = materials.add(Color::from(tailwind::TEAL_200));
+
+    commands.spawn((
+        Player,
+        CameraSensitivity::default(),
+        Transform::from_xyz(0.0, 1.0, 0.0),
+        Visibility::default(),
+        children![
+            (
+                WorldModelCamera,
+                Camera3d::default(),
+                Projection::from(PerspectiveProjection {
+                    fov: 90.0_f32.to_radians(),
+                    ..default()
+                }),
+            ),
+            (
+                Camera3d::default(),
+                Camera {
+                    order: 1,
+                    ..default()
+                },
+                Projection::from(PerspectiveProjection {
+                    fov: 70.0_f32.to_radians(),
+                    ..default()
+                }),
+                RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
+            ),
+            (
+                Mesh3d(arm),
+                MeshMaterial3d(arm_material),
+                Transform::from_xyz(0.2, -0.1, -0.5).with_rotation(Quat::from_rotation_x(PI / 3.0)),
+                RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
+                NotShadowCaster,
+            ),
+        ],
+    ));
+}
+
+fn mouse_movement(
+    accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
+    player: Single<(&mut Transform, &CameraSensitivity), With<Player>>,
+) {
+    let (mut transform, camera_sensitivity) = player.into_inner();
+    let delta = accumulated_mouse_motion.delta;
+
+    if delta != Vec2::ZERO {
+        let delta_yaw = -delta.x * camera_sensitivity.x;
+        let delta_pitch = -delta.y * camera_sensitivity.y;
+
+        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
+        let yaw = yaw + delta_yaw;
+        const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
+        let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
+        transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+    }
+}
