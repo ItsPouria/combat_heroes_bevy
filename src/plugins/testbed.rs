db554826955ceb3e2216c47911e01ef67efdd544
ItsPouria <pouriamemarvar@gmail.com>
2026-02-01 02:00:31 +0100

    add testbed plugin to spawn a test ground scene

---
+++ src/plugins/testbed.rs
@@ -0,0 +1,50 @@
+use avian3d::prelude::{Collider, RigidBody};
+use bevy::{camera::visibility::RenderLayers, color::palettes::css::SILVER, prelude::*};
+
+use crate::plugins::player::VIEW_MODEL_RENDER_LAYER;
+
+pub struct Testbed;
+
+impl Plugin for Testbed {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, (setup_testbed, spawn_light));
+    }
+}
+pub static DEFAULT_RENDER_LAYER: usize = 0;
+
+fn setup_testbed(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let floor = Plane3d::default().mesh().size(50.0, 50.0).subdivisions(10);
+    let outward_normal = Vec3 {
+        x: 0.0,
+        y: 1.0,
+        z: 0.0,
+    };
+    commands.spawn((
+        Mesh3d(meshes.add(floor)),
+        MeshMaterial3d(materials.add(Color::from(SILVER))),
+        children![
+            RigidBody::Dynamic,
+            Collider::half_space(outward_normal),
+            Transform::from_xyz(0.0, 3.0, 0.0),
+        ],
+    ));
+}
+
+fn spawn_light(mut commands: Commands) {
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            intensity: 10_000_000.,
+            range: 100.0,
+            shadow_depth_bias: 0.2,
+            ..default()
+        },
+        Transform::from_xyz(8.0, 16.0, 8.0),
+        RenderLayers::from_layers(&[DEFAULT_RENDER_LAYER, VIEW_MODEL_RENDER_LAYER]),
+    ));
+}
+
